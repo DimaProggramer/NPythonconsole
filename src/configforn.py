4d83942a349bae00b3e78f0594b3f46d81436b57
DimaProggramer <135269406+DimaProggramer@users.noreply.github.com>
2024-06-14 21:45:36 +0300

    Create configforn.py

---
+++ src/configforn.py
@@ -0,0 +1,63 @@
+RESET = "\033[0m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+BRIGHT_BLACK = "\033[90m"
+BRIGHT_RED = "\033[91m"
+BRIGHT_GREEN = "\033[92m"
+BRIGHT_YELLOW = "\033[93m"
+BRIGHT_BLUE = "\033[94m"
+BRIGHT_MAGENTA = "\033[95m"
+BRIGHT_CYAN = "\033[96m"
+BRIGHT_WHITE = "\033[97m"
+PINK = "\033[38;5;213m"
+ORANGE = "\033[38;5;214m"
+LIGHT_GREEN = "\033[38;5;120m"
+LIGHT_BLUE = "\033[38;5;123m"
+LIGHT_MAGENTA = "\033[38;5;207m"
+LIGHT_CYAN = "\033[38;5;159m"
+LIGHT_GRAY = "\033[38;5;250m"
+DARK_GRAY = "\033[38;5;240m"
+symbols = "Test"
+print(f"{BLACK}{symbols}{RESET}")
+print(f"{RED}{symbols}{RESET}")
+print(f"{GREEN}{symbols}{RESET}")
+print(f"{YELLOW}{symbols}{RESET}")
+print(f"{BLUE}{symbols}{RESET}")
+print(f"{MAGENTA}{symbols}{RESET}")
+print(f"{CYAN}{symbols}{RESET}")
+print(f"{WHITE}{symbols}{RESET}")
+print(f"{BRIGHT_BLACK}{symbols}{RESET}")
+print(f"{BRIGHT_RED}{symbols}{RESET}")
+print(f"{BRIGHT_GREEN}{symbols}{RESET}")
+print(f"{BRIGHT_YELLOW}{symbols}{RESET}")
+print(f"{BRIGHT_BLUE}{symbols}{RESET}")
+print(f"{BRIGHT_MAGENTA}{symbols}{RESET}")
+print(f"{BRIGHT_CYAN}{symbols}{RESET}")
+print(f"{BRIGHT_WHITE}{symbols}{RESET}")
+print(f"{PINK}{symbols}{RESET}")
+print(f"{ORANGE}{symbols}{RESET}")
+print(f"{LIGHT_GREEN}{symbols}{RESET}")
+print(f"{LIGHT_BLUE}{symbols}{RESET}")
+print(f"{LIGHT_MAGENTA}{symbols}{RESET}")
+print(f"{LIGHT_CYAN}{symbols}{RESET}")
+print(f"{LIGHT_GRAY}{symbols}{RESET}")
+print(f"{DARK_GRAY}{symbols}{RESET}")
+logo = """ _   _   __
+| '_' (_)\ \  
+| | | |_ / /
+|_| |_(_)_/""" #dont change pls
+cat = """                           _╱|、
+                          (•ˎ . 7  
+                          |、˜〵          
+                          じしˍ,)ノ
+"""
+colorc = GREEN
+show_path = False #show path (.../conpyt/home n:> if True)
+root = False
+# u can change this but careful
